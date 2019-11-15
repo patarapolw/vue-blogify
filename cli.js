b59f146427563542198e420f140de16fdd73908c
patarapolw <patarapolw@gmail.com>
2019-11-15 22:53:11 +0700

    add CLI

--- cli.js
+++ cli.js
@@ -1,7 +1,11 @@
+#!/usr/bin/env node
+
 const loadConfig = require("./load");
 const { spawnSync } = require("child_process");
 const program = require("commander");
+const fs = require("fs-extra");
 const pkg = require("./package.json");
+const path = require("path");
 
 program
   .version(pkg.version)
@@ -9,15 +13,24 @@ program
 
 program.parse(process.argv);
 
+const blogify = loadConfig();
+
+if (!fs.existsSync(path.join(blogify.themeDir, "node_modules"))) {
+  console.log("Installing theme")
+
+  spawnSync("npm", ["install"], {
+    cwd: blogify.themeDir,
+    stdio: "inherit"
+  });
+}
+
 if (program.build) {
-  const blogify = loadConfig();
-  spawnSync("npm run build", {
+  spawnSync("npm", ["run", "build"], {
     cwd: blogify.themeDir,
     stdio: "inherit"
   });
 } else {
-  const blogify = loadConfig();
-  spawnSync("npm run serve", {
+  spawnSync("npm", ["run", "serve"], {
     cwd: blogify.themeDir,
     stdio: "inherit"
   });
